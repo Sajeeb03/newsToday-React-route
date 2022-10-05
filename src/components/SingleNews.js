122003b701fc16dc06fb23a4e8f2aa87159b7e7d
Sajeb03 <sajeebmuntasir0@gmail.com>
2022-10-05 12:58:27 +0600

    news added

--- src/components/SingleNews.js
+++ src/components/SingleNews.js
@@ -4,10 +4,13 @@ const SingleNews = ({ singleNews }) => {
     console.log(singleNews);
     const { author, details, image_url, thumbnail_url, total_view, title } = singleNews;
     return (
-        <div className='flex border-2 border-red-200 gap-3 m-3 shadow-lg rounded-lg p-3'>
+        <div className='flex border-2 border-red-200 gap-3 m-3 shadow-lg rounded-lg p-3 relative'>
             <img src={thumbnail_url} alt="" />
             <div>
                 <h1 className='text-4xl font-bold'>{title}</h1>
+                <p><span>{author.name ? author.name : 'no name'}</span> <span>Views:{total_view}</span></p>
+                <p>{details.length > 300 ? details.slice(0, 300) : details}...</p>
+                <button className='bg-blue-300 p-3 rounded absolute bottom-1 right-1'>Read More</button>
             </div>
         </div>
     );
